59f897c3a59b9a0af991eb5ec1b59015033e7700
Wladimir <weizen.w@gmx.de>
2023-05-01 17:09:12 +0200

    add Exception

--- src/VocabularyIT.java
+++ src/VocabularyIT.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -63,8 +64,10 @@ public class VocabularyIT {
         hashMap.put(word, value);
       }
       bufferedReader.close();
+      return hashMap;
+    } else {
+      throw new FileNotFoundException("File not found: " + file);
     }
-    return hashMap;
   }
 
   private static void search(List<String> list, Map<String, String> map) {
