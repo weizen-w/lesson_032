1ce496f56e83008b8818fa5ba9803afd52c0b6ca
Wladimir <weizen.w@gmx.de>
2023-05-01 14:59:39 +0200

    add reading a file files.txt

--- src/FilePermissions.java
+++ src/FilePermissions.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FilePermissions {
 //  В файловую систему одного суперкомпьютера проник вирус, который сломал контроль за правами
 //  доступа к файлам. Для каждого файла известно, с какими действиями можно к нему обращаться:
@@ -40,4 +47,21 @@ public class FilePermissions {
 //  pinglog: read: OK
 //  pinglog: write: OK
 
+  public static void main(String[] args) throws IOException {
+    Map<String, String[]> files = new HashMap<>();
+    BufferedReader bufferedReader = new BufferedReader(new FileReader("res/files.txt"));
+    if (bufferedReader.ready()) {
+      int numberFiles = Integer.parseInt(bufferedReader.readLine());
+      for (int i = 0; i < numberFiles; i++) {
+        String line = bufferedReader.readLine();
+        int firstSpaceIndex = line.indexOf(" ");
+        String nameFile = line.substring(0, firstSpaceIndex);
+        String restLine = line.substring(firstSpaceIndex + 1);
+        files.get(nameFile);
+        String[] actions = restLine.split(" ");
+        files.put(nameFile, actions);
+      }
+      bufferedReader.close();
+    }
+  }
 }
